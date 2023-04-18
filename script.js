1d005179596e4f000f34d3d25e1969f3b2a1bc81
joemiddie13 <joemiddie13@gmail.com>
2023-04-17 23:37:19 -0700

    Created HTML file to work through JavaScript

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 // An array of car automobile companies
-const cars = ["Rivian", "Jeep", "Ford"];
+// const cars = ["Rivian", "Jeep", "Ford"];
 // Arrays are a much more efficient way to create variables
 // What if you had 300 different cars? Arrays, baby.
 
 // It's ok to create arrays on multiple lines as well:
-const popsicle = [
-    "Mango",
-    "Strawberry"
-    "Watermelon"
-];
+// const popsicle = [
+//     "Mango",
+//     "Strawberry"
+//     "Watermelon"
+// ];
+
+// Here you can create an array and then provide or assign the elements [1]
+// const cars = [];
+// cars[0] = "Rivian";
+// cars[1] = "Jeep";
+// cars[2] = "Ford";
+
+// Accessing Array Elements
+
+const cars = ["Rivian", "Jeep", "Ford"];
+let car = cars[0];
